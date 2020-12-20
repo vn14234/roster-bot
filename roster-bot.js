46b55518388768a2ab39be352ebb0b3a4f088c3d
vn14234 <vn14234@github.com>
2020-12-20 17:19:53 +0100

    read token from command line

--- roster-bot.js
+++ roster-bot.js
@@ -3,20 +3,12 @@ var Jimp = require('jimp');
 var Canvas = require('canvas');
 var waterfall = require('async-waterfall');
 const fs = require('fs')
-
-// Import the discord.js library.
 const Discord = require("discord.js")
-// Create a new discord.js client.
+
 const bot = new Discord.Client()
 
-const DIG = require("discord-image-generation");
-
-var ua = "User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:84.0) Gecko/20100101 Firefox/84.0"
-
-var options = {
-	userAgent: ua
-};
-
+var args = process.argv.slice(2);
+token = args[0]
 
 bot.on("ready", () => {
 	console.log("bot is ready...");
@@ -40,15 +32,14 @@ function deleteFile(file) {
 
 // Listen to the message event
 bot.on("message", async (message) => {
-	if (message.content === "*update") {		
+	if (message.content === "*update") {
 
-		if(!message.member.roles.cache.some(role => role.name === 'BWL-Recruiter'))
-		{
+		if (!message.member.roles.cache.some(role => role.name === 'BWL-Recruiter')) {
 			console.log("*update... => rejected")
 			return
 		}
 
-		console.log("*update... => " +message.author.username+ "["+message.author.tag+"]@(" + message.guild.name+")")
+		console.log("*update... => " + message.author.username + "[" + message.author.tag + "]@(" + message.guild.name + ")")
 
 		message.delete()
 		var reply = message.reply("posting roster...")
@@ -57,7 +48,7 @@ bot.on("message", async (message) => {
 					timeout: 10000
 				})
 			})
-			.catch( console.log("failed to reply...") )
+			.catch(console.log("failed to reply..."))
 
 		deleteFile('sheet.png')
 		deleteFile('cropped_sheet.png')
@@ -68,6 +59,10 @@ bot.on("message", async (message) => {
 			waterfall(
 				[
 					function(callback) {
+						var ua = "User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:84.0) Gecko/20100101 Firefox/84.0"
+						var options = {
+							userAgent: ua
+						};
 						webshot(url, 'sheet.png', options, function(err, reply) {
 							if (err) throw err;
 							callback(null)
@@ -89,7 +84,7 @@ bot.on("message", async (message) => {
 					function(callback) {
 						const attachment = new Discord.MessageAttachment('./cropped_sheet.png');
 						message.channel.send(attachment)
-						callback(null,"success")
+						callback(null, "success")
 					}.bind({
 						message: message
 					})
@@ -106,4 +101,4 @@ bot.on("message", async (message) => {
 	}
 })
 
-bot.login("Njg2MjU5MjY4NDM2ODg1NTIy.XmUmvw.HRIigv6xnyQ7ievnL6SaYsUoh54")
+bot.login(token)
